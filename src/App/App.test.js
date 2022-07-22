0e32a80997f7d75cc437726edfe148924d004973
Salma Badr <salmabadr00@gmail.com>
2022-07-22 17:19:17 +0200

    Updating App tests with header check

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,6 +1,8 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from ".";
 
 test("App Renders correctly", () => {
   render(<App />);
+  const header = screen.getByRole("banner");
+  expect(header).toBeInTheDocument();
 });
